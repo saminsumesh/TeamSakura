72573118c3e9dd7fca9c849084ec3d5308e37d3d
PaulWalker <91833744+Vampire421@users.noreply.github.com>
2021-10-30 22:41:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -547,8 +547,8 @@ async def auto_filter(client, message):
             )
         imdb=await get_poster(search)
         if imdb and imdb.get('poster'):
-            await message.reply_photo(photo=imdb.get('poster'), caption=f"<b>Query: {search}</b> \n‌IMDb Data:\n\n🏷 Title: <a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year: <a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n🖋 StoryLine: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_photo(photo=imdb.get('poster'), caption=f"<b>**Query**: {search}</b> \n‌**IMDB Data**:\n\n**🏷 Title**: <a href={imdb['url']}>{imdb.get('title')}</a>\n**🎭 Genres**: {imdb.get('genres')}\n**📆 Year**: <a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n**🌟 Rating**: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10\n**🖋 StoryLine**: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn))
         elif imdb:
-            await message.reply_text(f"<b>Query: {search}</b> \n‌IMDb Data:\n\n🏷 Title: <a href={imdb['url']}>{imdb.get('title')}</a>\n🎭 Genres: {imdb.get('genres')}\n📆 Year: <a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n🌟 Rating: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10 \n🖋 StoryLine: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn))
+            await message.reply_text(f"<b>**Query:** {search}</b> \n‌**IMDB Data**:\n\n**🏷 Title**: <a href={imdb['url']}>{imdb.get('title')}</a>\n**🎭 Genres**: {imdb.get('genres')}\n**📆 Year**: <a href={imdb['url']}/releaseinfo>{imdb.get('year')}</a>\n**🌟 Rating**: <a href={imdb['url']}/ratings>{imdb.get('rating')}</a> / 10 \n**🖋 StoryLine**: <code>{imdb.get('plot')} </code>", reply_markup=InlineKeyboardMarkup(btn))
         else:
             await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‎ </b>", reply_markup=InlineKeyboardMarkup(btn))
