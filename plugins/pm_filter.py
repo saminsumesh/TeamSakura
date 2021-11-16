aea3fc572f5a8d4bdb5a078cecafcd454c0f8650
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-16 19:00:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -93,7 +93,7 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"{get_size(file.file_size)} 🍭, {file.file_name} 🎪", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
