1ced176858457038ecd18cfd10b564ec905869c7
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-17 01:29:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -394,7 +394,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
