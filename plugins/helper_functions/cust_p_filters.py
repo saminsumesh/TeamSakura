b5f324f891d28156036de766f4ca970aa279adbe
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-11-17 15:25:04 +0530

    Update cust_p_filters.py

--- plugins/helper_functions/cust_p_filters.py
+++ plugins/helper_functions/cust_p_filters.py
@@ -1,12 +1,10 @@
 from pyrogram import (
     filters
 )
-from pyrobot import (
-    SUDO_USERS,
-    USE_AS_BOT
-)
+from info import ADMINS, is_enabled
 from plugins.helper_functions.admin_check import admin_check
 
+USE_AS_BOT = is_enabled(environ.get("USE_AS_BOT", "True"), True)
 
 def f_sudo_filter(filt, client, message):
     return bool(
@@ -23,7 +21,7 @@ sudo_filter = filters.create(
 def onw_filter(filt, client, message):
     if USE_AS_BOT:
         return bool(
-            True # message.from_user.id in SUDO_USERS
+            True # message.from_user.id in ADMINS
         )
     else:
         return bool(
