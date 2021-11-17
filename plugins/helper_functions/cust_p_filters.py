b4dc1c44e1aa40534353a87eea12bc3643009e94
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-17 11:34:42 +0530

    Update cust_p_filters.py

--- plugins/helper_functions/cust_p_filters.py
+++ plugins/helper_functions/cust_p_filters.py
@@ -6,6 +6,17 @@ from pyrogram import (
     filters
 )
 
+def onw_filter(filt, client, message):
+    if USE_AS_BOT:
+        return bool(
+            True # message.from_user.id in SUDO_USERS
+        )
+    else:
+        return bool(
+            message.from_user and
+            message.from_user.is_self
+        )
+
 async def admin_filter_f(filt, client, message):
     return await admin_check(message)
 
