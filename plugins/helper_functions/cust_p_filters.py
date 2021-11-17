637fdd974eca57b8b2ec9129558c383705516336
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-17 12:17:39 +0530

    Update cust_p_filters.py
    chumma

--- plugins/helper_functions/cust_p_filters.py
+++ plugins/helper_functions/cust_p_filters.py
@@ -5,7 +5,7 @@ from pyrobot import (
     SUDO_USERS,
     USE_AS_BOT
 )
-from pyrobot.helper_functions.admin_check import admin_check
+from plugins.helper_functions.admin_check import admin_check
 
 
 def f_sudo_filter(filt, client, message):
