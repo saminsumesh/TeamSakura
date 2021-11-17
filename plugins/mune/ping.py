84f8609420dca88d29dffabc5418d58c4760494f
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-18 01:55:41 +0530

    Create ping.py

---
+++ plugins/mune/ping.py
@@ -0,0 +1,36 @@
+"""Telegram Ping / Pong Speed
+Syntax: .ping"""
+
+import time
+from pyrogram import Client, filters
+from info import (
+    COMMAND_HAND_LER
+)
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+# -- Constants -- #
+ALIVE = "Not Dead"
+HELP = "CAADAgAD6AkAAowucAABsFGHedLEzeUWBA"
+# -- Constants End -- #
+
+
+@Client.on_message(
+    filters.command(["alive", "start"], COMMAND_HAND_LER) &
+    f_onw_fliter
+)
+async def check_alive(_, message):
+    await message.reply_text(ALIVE)
+
+
+@Client.on_message(filters.command("help", COMMAND_HAND_LER) & f_onw_fliter)
+async def help_me(_, message):
+    await message.reply_sticker(HELP)
+
+
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
