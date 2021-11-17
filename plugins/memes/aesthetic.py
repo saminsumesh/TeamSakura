1757f1e8c88c33123b4055888562ec218209cd78
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-17 23:41:36 +0530

    Create aesthetic.py

---
+++ plugins/memes/aesthetic.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+
+def aesthetify(string):
+    PRINTABLE_ASCII = range(0x21, 0x7f)
+    for c in string:
+        c = ord(c)
+        if c in PRINTABLE_ASCII:
+            c += 0xFF00 - 0x20
+        elif c == ord(" "):
+            c = 0x3000
+        yield chr(c)
+
+
+@Client.on_message(
+    filters.command(["ae"], COMMAND_HAND_LER) &
+    f_onw_fliter
+)
+async def aesthetic(client, message):
+    status_message = await message.reply_text("...")
+    text = "".join(str(e) for e in message.command[1:])
+    text = "".join(aesthetify(text))
+    await status_message.edit(text)
