7b5ebd9258c54e63000f3aad8f05148ad07b14e7
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-17 08:57:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,6 +24,7 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+COMMAND_HAND_LER = os.environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
