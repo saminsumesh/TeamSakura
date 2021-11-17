c360dc695ceec8e847c6635980e654b8108feb9d
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-17 19:36:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", "")
+
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
@@ -45,3 +45,6 @@ IMDB = bool((environ.get('IMDB', True)))
 SINGLE_BUTTON = bool((environ.get('SINGLE_BUTTON', False)))
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
 LONG_IMDB_DESCRIPTION = bool(environ.get("LONG_IMDB_DESCRIPTION", True))
+
+# sudi
+SUDO_USERS = set(int(x) for x in os.environ.get("SUDO_USERS", None")
