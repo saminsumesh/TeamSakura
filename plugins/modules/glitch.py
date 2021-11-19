fa91592194a76fd1dcae9e2fb1430baceda33f57
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-20 02:24:51 +0530

    Update glitch.py

--- plugins/modules/glitch.py
+++ plugins/modules/glitch.py
@@ -30,14 +30,22 @@ async def glitch_art(bot, update):
         await update.download(
             file_name=download_location
         )
+    except Exception as error:
         await message.edit_text(
-            text="`Converting to glitch...`"
+            text=f"**Error :** `{error}`\nContact @TheFayas."
+        )
+        return 
+    await message.edit_text(
+        text="`Converting to glitch...`"
     )
     try:
         glitch_art = glitchart.jpeg(download_location)
         await update.reply_photo(photo=glitch_art, quote=True)
         os.remove(download_location)
         os.remove(glitch_art)
+    except Exception as error:
+        await message.edit_text(
+            text=f"**Error :** `{error}`\nContact @TheFayas."
         )
         return
     await message.delete()
