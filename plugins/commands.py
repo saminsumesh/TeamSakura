16f7d819e431125020aa926c3ff9208ea14e3fda
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-19 13:19:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import requests
 import random
 from Script import script
 from pyrogram import Client, filters
@@ -12,6 +13,48 @@ from utils import get_size, is_subscribed, temp
 from telegraph import upload_file
 
 logger = logging.getLogger(__name__)
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/sakurabotupdates')]])
+
+API = "https://api.sumanjay.cf/covid/?country="
+
+START_TEXT = """Hello {}, just sent your Country
+Made by @SakuraBotUpdates ❤"""
+
+@Client.on_message(filters.private & filters.text & filters.command(["covid"]))
+async def reply_info(bot, update):
+    reply_markup = BUTTONS
+    await update.reply_text(
+        text=covid_info(update.text),
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=reply_markup
+    )
+
+
+def covid_info(country_name):
+    try:
+        r = requests.get(API + requote_uri(country_name.lower()))
+        info = r.json()
+        country = info['country'].capitalize()
+        active = info['active']
+        confirmed = info['confirmed']
+        deaths = info['deaths']
+        info_id = info['id']
+        last_update = info['last_update']
+        latitude = info['latitude']
+        longitude = info['longitude']
+        recovered = info['recovered']
+        covid_info = f"""**Covid 19 Information**
+**Country :** `{country}`
+**Actived :** `{active}`
+**Confirmed :** `{confirmed}`
+**Deaths :** `{deaths}`
+**ID :** `{info_id}`
+**Last Update :** `{last_update}`
+**Latitude :** `{latitude}`
+**Longitude :** `{longitude}`
+**Recovered :** `{recovered}`
+Made by @SakuraBotUpdates ❤"""        
 
 @Client.on_message(filters.command("start"))
 async def start(client, message):
