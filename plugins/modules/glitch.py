7f6c205ab2431e9f2226ea837ae4ac1f726d0033
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-20 02:52:56 +0530

    Update glitch.py

--- plugins/modules/glitch.py
+++ plugins/modules/glitch.py
@@ -3,23 +3,28 @@ import glitchart
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-
+START_BUTTONS = InlineKeyboardMarkup(
+    [[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+    ]]
+)
 PATH = os.environ.get("PATH", "./DOWNLOADS")
 
 @Client.on_message(filters.private & filters.command(["glitch"]))
 async def reply_info(bot, update):
     await update.reply_text(
-        text=text.format(update.from_user.mention),
+        text=.format(update.from_user.mention),
         reply_markup=START_BUTTONS,
         disable_web_page_preview=True,
         quote=True
     )
 
 
-@Client.on_message(filters.private & filters.photo)
+@Bot.on_message(filters.private & filters.photo)
 async def glitch_art(bot, update):
     download_path = PATH + "/" + str(update.from_user.id) + "/"
-    download_location = download_path + "image.jpg"
+    download_location = download_path + "photo.jpg"
     message = await update.reply_text(
         text="`Processing...`",
         quote=True
