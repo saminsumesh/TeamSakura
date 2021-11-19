f5388d5e7197da9a3498d13c590d20de2c8c7403
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-19 12:57:24 +0530

    Update yt_thumbs.py

--- plugins/modules/yt_thumbs.py
+++ plugins/modules/yt_thumbs.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇs', url='https://t.me/sakurabotupdates')]])
 
 Bot = Client(
-    "YouTube-Thumbnail-Downloader",
+    "YouTube-Thumbnail-Downloade",
     bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
