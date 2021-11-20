f8f0c2833a52fed00f2ab85f167407b48db7ac90
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-21 02:55:56 +0530

    Update you_tube_dl_extractor.py

--- plugins/helper_functions/you_tube_dl_extractor.py
+++ plugins/helper_functions/you_tube_dl_extractor.py
@@ -4,7 +4,7 @@
 
 
 import asyncio
-import aiohttp
+import aiofiles
 import json
 import os
 from PIL import Image
@@ -12,7 +12,7 @@ from pyrogram.types import (
     InlineKeyboardButton,
     InlineKeyboardMarkup
 )
-from pyrobot.helper_functions.display_progress_dl_up import humanbytes
+from plugins.helper_functions.display_progress_dl_up import humanbytes
 
 
 async def proc_ess_image_aqon(image_url: str, output_dir: str) -> str:
