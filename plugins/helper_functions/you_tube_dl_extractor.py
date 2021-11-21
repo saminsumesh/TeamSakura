28de0520a1c596a56d3d9675d44e36425e5c5eb3
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-11-21 10:34:33 +0530

    Update you_tube_dl_extractor.py

--- plugins/helper_functions/you_tube_dl_extractor.py
+++ plugins/helper_functions/you_tube_dl_extractor.py
@@ -42,7 +42,7 @@ async def extract_youtube_dl_formats(url, user_working_dir):
         "-j",
         url
     ]
-    if "hotstar" in url:
+    if "youtube" in url:
         command_to_exec.append("--geo-bypass-country")
         command_to_exec.append("IN")
     LOGGER.info(command_to_exec)
