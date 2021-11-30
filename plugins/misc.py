91805262945711793841dae7fb3b8a8719266180
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-01 03:51:52 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -203,7 +203,7 @@ async def help(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention),
+            caption=script.HELP_TXT.format(message.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
         )
