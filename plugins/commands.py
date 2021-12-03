c497fd37d0dee58a1a9b5265202772b281944e64
PaulWalker <91833744+saminsumesh@users.noreply.github.com>
2021-12-03 12:11:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,5 +1,4 @@
 import os
-import youtube_dl
 from youtube_search import YoutubeSearch
 import time
 import logging
@@ -194,69 +193,3 @@ async def delete_all_index_confirm(bot, message):
     await message.answer()
     await message.message.edit('Succesfully Deleted All The Indexed Files.')
 
-@Client.on_message(filters.command(['song']))
-def a(client, message):
-    query = ''
-    for i in message.command[1:]:
-        query += ' ' + str(i)
-    print(query)
-    m = message.reply('sᴇᴀʀᴄʜɪɴɢ..')
-    ydl_opts = {"format": "bestaudio[ext=m4a]"}
-    try:
-        results = []
-        count = 0
-        while len(results) == 0 and count < 6:
-            if count>0:
-                time.sleep(1)
-            results = YoutubeSearch(query, max_results=1).to_dict()
-            count += 1
-        # results = YoutubeSearch(query, max_results=1).to_dict()
-        try:
-            link = f"https://youtube.com{results[0]['url_suffix']}"
-            # print(results)
-            title = results[0]["title"]
-            thumbnail = results[0]["thumbnails"][0]
-            duration = results[0]["duration"]
-            views = results[0]["views"]
-
-            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
-            # if time_to_seconds(duration) >= 7000:  # duration limit
-            #     m.edit("Exceeded 30mins cap")
-            #     return
-
-            performer = f"sᴀᴋᴜʀᴀғɪʟᴛᴇʀʙᴏᴛ" 
-            thumb_name = f'thumb{message.message_id}.jpg'
-            thumb = requests.get(thumbnail, allow_redirects=True)
-            open(thumb_name, 'wb').write(thumb.content)
-
-        except Exception as e:
-            print(e)
-            m.edit('ɴᴏᴛʜɪɴɢ ғᴏᴜɴᴅ ʀᴇᴛʀʏ ᴡɪᴛʜ ᴀɴᴏᴛʜᴇʀ')
-            return
-    except Exception as e:
-        m.edit(
-            "ᴇɴᴛᴇʀ sᴏɴɢ ɴᴀᴍᴇ ᴡɪᴛʜ /song ᴄᴏᴍᴍᴀɴᴅ!"
-        )
-        print(str(e))
-        return
-    m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ....")
-    try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-        rep = f'▪️ 𝖳𝗂𝗍𝗅𝖾: <a href="{link}">{title}</a>\n🧭 𝖣𝗎𝗋𝖺𝗍𝗂𝗈𝗇: <code>{duration}</code>\n❤️ 𝖡𝖾𝗁𝗂𝗇𝖽 𝖳𝗁𝗂𝗌: <a href="https://t.me/XD_Botz">XD Botz</a>'
-        secmul, dur, dur_arr = 1, 0, duration.split(':')
-        for i in range(len(dur_arr)-1, -1, -1):
-            dur += (int(dur_arr[i]) * secmul)
-            secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
-        m.delete()
-    except Exception as e:
-        m.edit('ᴀɴ ɪɴᴛᴇʀɴᴀʟ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀᴇᴅ, ʀᴇᴘᴏʀᴛ ᴛʜɪs @SakuraBotSupport ❤️')
-        print(e)
-    try:
-        os.remove(audio_file)
-        os.remove(thumb_name)
-    except Exception as e:
-        print(e)
